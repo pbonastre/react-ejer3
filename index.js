6d8bfb57fdb4efeba95207139ecc7492f8b407ea
pbonastre <bonastrep@gmail.com>
2019-05-07 13:15:49 +0200

    constantes y componentes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-function Max(props {
+const Hello = (props) => <h2>Bievenido a React </h2>
+function Max(props) {
   return <h2>Max of 2 and 5 is </h2>
-})
+}
 
 class App extends Component {
   constructor() {
@@ -17,6 +18,7 @@ class App extends Component {
     return (
       <div>
         <Hello name='Susan'/>
+        <Max />
        </div>
     );
   }
