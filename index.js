82cdc7c17b5af54ac40a270bf3df76fb8dbd3d05
pbonastre <bonastrep@gmail.com>
2019-05-07 13:34:55 +0200

    añadidos mas ejemplos

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const Hello = (props) => <h2>Bievenido a React </h2>
+const Hello = (props) => {
+  //lo evalua como cadena undefined= false, todas las cadenas las pone como true.(false sale cuando =  cero, undefined, cero y cadena vacia)
+  //El ordenador evalua por orden, si el primero ya es true, pondrá ese, el orden importa
+  return  (<h2>Bievenida {props.name||'nobody'} a React </h2>)
+}
+
+//Esto peta,pq no podemos cambiar las variables
+//let Hello = (props) =>{
+ // props.name ='jimena'
+//  return (<h2>{props.name} bievenida a React </h2>)
+//}
+
 function Max(props) {
   //Entre llaves se pueden poner expresiones (algo que devuelve algo)en javascript 
   return <h2>Max of 2 and 5 is {Math.max(2,5)}</h2>
@@ -18,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name='Susan'/>
+        <Hello />
         <Max />
        </div>
     );
