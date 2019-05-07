3822a35ba4a21b82c265f9dfb9fa0fbe9d9c78b5
pbonastre <bonastrep@gmail.com>
2019-05-07 13:18:40 +0200

    añadido expresiones

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { render } from 'react-dom';
 
 const Hello = (props) => <h2>Bievenido a React </h2>
 function Max(props) {
-  return <h2>Max of 2 and 5 is </h2>
+  //Entre llaves se pueden poner expresiones (algo que devuelve algo)en javascript 
+  return <h2>Max of 2 and 5 is {Math.max(2,5)}</h2>
 }
 
 class App extends Component {
